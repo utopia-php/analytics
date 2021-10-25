ef0f530654ccce963fd4d295d0f339dfa1ee77d2
Loveena Saran <42793632+Pretty-19@users.noreply.github.com>
2021-10-25 12:48:03 +0530

    Update src/Analytics/Plausible.php
    property to pros attributers
    
    Co-authored-by: Torsten Dittmann <torsten.dittmann@googlemail.com>

--- src/Analytics/Plausible.php
+++ src/Analytics/Plausible.php
@@ -51,7 +51,7 @@ class  PlausibleAdapter extends Analytics
 
         $query = [
             'url' => $url,
-            'poperty' => $property,
+            'props' => $property,
             'domain' =>$domain,
             'name' => 'event'
         ];
