fa92d882797477e6600fd0560428897e22fd8c7d
Bradley Schofield <ionicisere@gmail.com>
2022-02-09 15:01:05 +0000

    Remove other adapters

--- tests/Analytics/AnalyticsTest.php
+++ tests/Analytics/AnalyticsTest.php
@@ -16,8 +16,6 @@ namespace Utopia\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Utopia\Analytics\Adapter\GoogleAnalytics;
-use Utopia\Analytics\Adapter\Plausible;
-
 class AnalyticsTest extends TestCase
 {
     public $ga;
@@ -26,7 +24,6 @@ class AnalyticsTest extends TestCase
     public function setUp(): void
     {
         $this->ga = new GoogleAnalytics("UA-132779463-1", "test");
-        $this->plausible = new Plausible("test");
     }
 
     public function testGoogleAnalytics()
@@ -37,15 +34,5 @@ class AnalyticsTest extends TestCase
         $this->ga->disable();
         $this->assertFalse($this->ga->createPageView("appwrite.io", "/docs/installation"));
         $this->assertFalse($this->ga->createEvent("testEvent", "testEvent"));
-    }
-
-    public function testPlausible()
-    {
-        $this->assertTrue($this->plausible->createPageView("appwrite.io", "/docs/installation"));
-        $this->assertTrue($this->plausible->createEvent("testEvent", "testEvent"));
-
-        $this->plausible->disable();
-        $this->assertFalse($this->plausible->createPageView("appwrite.io", "/docs/installation"));
-        $this->assertFalse($this->plausible->createEvent("testEvent", "testEvent"));
     }
 }
