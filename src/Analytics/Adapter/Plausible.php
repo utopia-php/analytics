e849cb68cee83f29bcbbaecc59e094220a628164
Bradley Schofield <ionicisere@gmail.com>
2022-02-11 10:25:20 +0000

    Refactor Plausible Adapter

---
+++ src/Analytics/Adapter/Plausible.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * Utopia PHP Framework
+ *
+ * @package Analytics
+ * @subpackage Tests
+ *
+ * @link https://github.com/utopia-php/framework
+ * @version 1.0 RC1
+ * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+namespace Utopia\Analytics\Adapter;
+
+use Utopia\Analytics\Adapter;
+use Utopia\Analytics\Event;
+
+class Plausible extends Adapter
+{
+    /**
+     *  Endpoint for Plausible
+     *  @var string
+     */
+    protected string $endpoint = 'https://plausible.io/api/event';
+
+    /**
+     * Useragent to use for requests
+     * @var string
+     */
+    protected string $userAgent = 'Utopia PHP Framework';
+
+    /**
+     * Headers to use for events
+     * @var array
+     */
+    protected array $headers;
+
+    /**
+     * Domain to use for events
+     * 
+     * @var string
+     */
+    protected string $domain;
+    
+
+    /**
+     * Gets the name of the adapter.
+     * 
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'GoogleAnalytics';
+    }
+
+    /**
+     * @param string $clientIP
+     * Specifies the  client's IP address used for CLI mode
+     * 
+     * @return Plausible
+     */
+
+    public function __construct(string $domain, string $useragent, string $clientIP = '127.0.0.1')
+    {
+        $this->domain = $domain;
+
+        $this->userAgent = $useragent;
+
+        $this->headers = array(' X_FORWARDED_FOR: '  . $clientIP, ' Content-Type: application/json ');
+    }
+
+    /**
+     * Sends an event to Plausible.
+     * 
+     * @param string $url
+     * URL of the page where the event was triggered.
+     * 
+     * @param string $domain
+     * Domain name of the site in Plausible.
+     *
+     * @param string $property
+     * Passes data to the event 
+     * 
+     * @return bool
+     */
+    public function createEvent(Event $event): bool
+    {
+        if (!$this->enabled) {
+            return false;
+        }
+
+        $query = [
+            'url' => $event->getUrl(),
+            'props' => $event->getProps(),
+            'domain' => $this->domain,
+            'name' => 'event'
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->domain);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
+    
+        curl_exec($ch);
+        curl_close($ch);
+    
+        return true;
+    }
+}
--- tests/Analytics/AnalyticsTest.php
+++ tests/Analytics/AnalyticsTest.php
@@ -15,7 +15,6 @@
 namespace Utopia\Tests;
 
 use PHPUnit\Framework\TestCase;
-
 use Utopia\Analytics\Adapter\GoogleAnalytics;
 use Utopia\Analytics\Event;
 use Utopia\Analytics\Adapter\Plausible;
@@ -27,7 +26,7 @@ class AnalyticsTest extends TestCase
     public function setUp(): void
     {
         $this->ga = new GoogleAnalytics("UA-XXXXXXXXX-X", "test");
-        $this->pa = new Plausible("UA-XXXXXXXXX-X", "test");
+        $this->pa = new Plausible("testdomain", "UA-XXXXXXXXX-X", "test");
     }
 
     public function testGoogleAnalytics()
@@ -55,11 +54,24 @@ class AnalyticsTest extends TestCase
 
     public function testPlausible()
     {
-        $this->assertTrue($this->pa->createPageView("appwrite.io", "/docs/installation"));
-        $this->assertTrue($this->pa->createEvent("testEvent", "testEvent"));
+        $pageviewEvent = new Event();
+        $pageviewEvent
+            ->setType('pageview')
+            ->setName('pageview')
+            ->setUrl('https://www.appwrite.io/docs/installation');
 
-        $this->ga->disable();
-        $this->assertFalse($this->pa->createPageView("appwrite.io", "/docs/installation"));
-        $this->assertFalse($this->pa->createEvent("testEvent", "testEvent"));
+        $normalEvent = new Event();
+        $normalEvent->setType('testEvent')
+            ->setName('testEvent')
+            ->setValue('testEvent')
+            ->setUrl('https://www.appwrite.io/docs/installation')
+            ->setProps(['category' => 'testEvent']);;
+
+        $this->assertTrue($this->pa->createEvent($pageviewEvent));
+        $this->assertTrue($this->pa->createEvent($normalEvent));
+
+        $this->pa->disable();
+        $this->assertFalse($this->pa->createEvent($pageviewEvent));
+        $this->assertFalse($this->pa->createEvent($normalEvent));
     }
 }
