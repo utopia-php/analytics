3126bd52cdb275427a8f06a7fc4c86120e269558
Bradley Schofield <ionicisere@gmail.com>
2022-02-14 11:32:02 +0000

    Add Orbit Adapter

--- src/Analytics/Adapter/Orbit.php
+++ src/Analytics/Adapter/Orbit.php
@@ -74,19 +74,24 @@ class Orbit extends Adapter
         $query = [
             'title' => $event->getName(),
             'activity_type_key' => $event->getType(),
-            'link' => $event->getUrl()
+            'link' => $event->getUrl(),
+            'identity' => [
+                'source' => 'uid',
+                'uid' => $event->getProp('uid')
+            ]
         ];
         
         $query = array_filter($query, fn($value) => !is_null($value) && $value !== '');
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->endpoint);
+        curl_setopt($ch, CURLOPT_URL, $this->endpoint . '/' . $this->workspace . '/activities');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5); 
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json'
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $this->key
         ]);
         curl_setopt(
             $ch,
--- tests/Analytics/AnalyticsTest.php
+++ tests/Analytics/AnalyticsTest.php
@@ -17,6 +17,7 @@ namespace Utopia\Tests;
 use PHPUnit\Framework\TestCase;
 
 use Utopia\Analytics\Adapter\GoogleAnalytics;
+use Utopia\Analytics\Adapter\Orbit;
 use Utopia\Analytics\Event;
 
 class AnalyticsTest extends TestCase
@@ -26,6 +27,7 @@ class AnalyticsTest extends TestCase
     public function setUp(): void
     {
         $this->ga = new GoogleAnalytics("UA-XXXXXXXXX-X", "test");
+        $this->orbit = new Orbit("workspacename", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
     }
 
     public function testGoogleAnalytics()
@@ -49,5 +51,30 @@ class AnalyticsTest extends TestCase
         $this->ga->disable();
         $this->assertFalse($this->ga->createEvent($pageviewEvent));
         $this->assertFalse($this->ga->createEvent($normalEvent));
+    }
+
+    public function testOrbit()
+    {
+        $pageviewEvent = new Event();
+        $pageviewEvent
+            ->setType('pageview')
+            ->setName('pageview')
+            ->addProp('uid', 'test')
+            ->setUrl('https://www.appwrite.io/docs/installation');
+
+        $normalEvent = new Event();
+        $normalEvent->setType('testEvent')
+            ->setName('testEvent')
+            ->setValue('testEvent')
+            ->addProp('uid', 'test')
+            ->addProp('category', 'testEvent')
+            ->setUrl('https://www.appwrite.io/docs/installation');
+
+        $this->assertTrue($this->orbit->createEvent($pageviewEvent));
+        $this->assertTrue($this->orbit->createEvent($normalEvent));
+
+        $this->orbit->disable();
+        $this->assertFalse($this->orbit->createEvent($pageviewEvent));
+        $this->assertFalse($this->orbit->createEvent($normalEvent));
     }
 }
