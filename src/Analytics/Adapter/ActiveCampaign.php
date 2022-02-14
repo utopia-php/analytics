a4c8944c7c5b566280fd2cabdd04e8f52a3c1300
Bradley Schofield <ionicisere@gmail.com>
2022-02-14 13:38:53 +0000

    Initial Implementation of Active Campaign adapter

---
+++ src/Analytics/Adapter/ActiveCampaign.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Utopia PHP Framework
+ *
+ * @package Analytics
+ * @subpackage Tests
+ *
+ * @link https://github.com/utopia-php/framework
+ * @author Torsten Dittmann <torsten@appwrite.io>
+ * @version 1.0 RC1
+ * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+namespace Utopia\Analytics\Adapter;
+
+use Utopia\Analytics\Adapter;
+use Utopia\Analytics\Event;
+
+class ActiveCampaign extends Adapter
+{
+    /**
+     *  Endpoint for ActiveCampaign
+     *  @var string
+     */
+    public string $endpoint = 'https://trackcmp.net/event';
+
+    /**
+     * Event Key for ActiveCampaign
+     * @var string
+     */
+    private string $key;
+
+    /**
+     * ActiveCampaign actid.
+     * @var string
+     */
+    private string $actid;
+
+    /**
+     * Gets the name of the adapter.
+     * 
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'ActiveCampaign';
+    }
+
+    /**
+     * @param string $key 
+     * Event Key for ActiveCampaign.
+     * 
+     * @param string $actid
+     * ActiveCampaign actid
+     * 
+     * @return ActiveCampaign
+     */
+    public function __construct(string $key, string $actid)
+    {
+        $this->key = $key;
+        $this->actid = $actid;
+    }
+
+    /**
+     * Creates an Event on the remote analytics platform.
+     * 
+     * @param Event $event
+     * @return bool
+     */
+    public function createEvent(Event $event): bool 
+    {
+        if (!$this->enabled) {
+            return false;
+        }
+
+        $query = [
+            'key' => $this->key,
+            'event' => $event->getName(),
+            'actid' => $this->actid,
+            'eventdata' => json_encode($event->getProps()),
+            'visit' => json_encode(['uid' => $event->getProp('uid')]),
+        ];
+        
+        $query = array_filter($query, fn($value) => !is_null($value) && $value !== '');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->endpoint);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5); 
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/x-www-form-urlencoded'
+        ]);
+        curl_setopt(
+            $ch,
+            CURLOPT_POSTFIELDS,
+            http_build_query($query)
+        );
+
+        $body = curl_exec($ch);
+
+        if (curl_error($ch) !== '') {
+            return false;
+        }
+
+        var_dump($body);
+
+        curl_close($ch);
+
+        return true;
+    }
+}
--- tests/Analytics/AnalyticsTest.php
+++ tests/Analytics/AnalyticsTest.php
@@ -15,7 +15,7 @@
 namespace Utopia\Tests;
 
 use PHPUnit\Framework\TestCase;
-
+use Utopia\Analytics\Adapter\ActiveCampaign;
 use Utopia\Analytics\Adapter\GoogleAnalytics;
 use Utopia\Analytics\Event;
 
@@ -26,6 +26,7 @@ class AnalyticsTest extends TestCase
     public function setUp(): void
     {
         $this->ga = new GoogleAnalytics("UA-XXXXXXXXX-X", "test");
+        $this->ac = new ActiveCampaign("99c8d61b2045ae8e2da13477c2276d76e52ceae5", "611851766");
     }
 
     public function testGoogleAnalytics()
@@ -49,5 +50,30 @@ class AnalyticsTest extends TestCase
         $this->ga->disable();
         $this->assertFalse($this->ga->createEvent($pageviewEvent));
         $this->assertFalse($this->ga->createEvent($normalEvent));
+    }
+
+    public function testActiveCampaign()
+    {
+        $pageviewEvent = new Event();
+        $pageviewEvent
+            ->setType('pageview')
+            ->setName('pageview')
+            ->addProp('uid', 'test')
+            ->setUrl('https://www.appwrite.io/docs/installation');
+
+        $normalEvent = new Event();
+        $normalEvent->setType('testEvent')
+            ->setName('testEvent')
+            ->setValue('testEvent')
+            ->addProp('category', 'testEvent')
+            ->addProp('uid', 'test')
+            ->setUrl('https://www.appwrite.io/docs/installation');
+
+        $this->assertTrue($this->ac->createEvent($pageviewEvent));
+        $this->assertTrue($this->ac->createEvent($normalEvent));
+
+        $this->ac->disable();
+        $this->assertFalse($this->ac->createEvent($pageviewEvent));
+        $this->assertFalse($this->ac->createEvent($normalEvent));
     }
 }
