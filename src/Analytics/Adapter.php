c051737db585af6f13fba016d96e1097eb88d3ee
Bradley Schofield <ionicisere@gmail.com>
2022-04-26 15:16:29 +0100

    Use generic call method

--- src/Analytics/Adapter.php
+++ src/Analytics/Adapter.php
@@ -14,6 +14,8 @@
 
 namespace Utopia\Analytics;
 
+use Exception;
+
 abstract class Adapter
 {
     protected bool $enabled = true;
@@ -24,6 +26,15 @@ abstract class Adapter
      * @return string
      */
     abstract public function getName(): string;
+
+    /**
+     * Global Headers
+     *
+     * @var array
+     */
+    protected $headers = [
+        'content-type' => '',
+    ];
 
     /**
      * Enables tracking for this instance.
@@ -52,5 +63,119 @@ abstract class Adapter
      * @return bool
      */
     abstract public function createEvent(Event $event): bool;
+
+    /**
+     * Call
+     *
+     * Make an API call
+     *
+     * @param string $method
+     * @param string $path
+     * @param array $params
+     * @param array $headers
+     * @return array|string
+     * @throws Exception
+     */
+    public function call($method, $path = '', $headers = array(), array $params = array())
+    {
+        $headers            = array_merge($this->headers, $headers);
+        $ch                 = curl_init((str_contains($path, 'http') ? $path : $this->endpoint . $path . (($method == 'GET' && !empty($params)) ? '?' . http_build_query($params) : '')));
+        $responseHeaders    = [];
+        $responseStatus     = -1;
+        $responseType       = '';
+        $responseBody       = '';
+
+        switch ($headers['content-type']) {
+            case 'application/json':
+                $query = json_encode($params);
+                break;
+
+            case 'multipart/form-data':
+                $query = $this->flatten($params);
+                break;
+
+            default:
+                $query = http_build_query($params);
+                break;
+        }
+
+        foreach ($headers as $i => $header) {
+            $headers[] = $i . ':' . $header;
+            unset($headers[$i]);
+        }
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, php_uname('s') . '-' . php_uname('r') . ':php-' . phpversion());
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, function($curl, $header) use (&$responseHeaders) {
+            $len = strlen($header);
+            $header = explode(':', strtolower($header), 2);
+
+            if (count($header) < 2) { // ignore invalid headers
+                return $len;
+            }
+
+            $responseHeaders[strtolower(trim($header[0]))] = trim($header[1]);
+
+            return $len;
+        });
+
+        if($method != 'GET') {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+        }
+
+        $responseBody   = curl_exec($ch);
+        $responseType   = $responseHeaders['content-type'] ?? '';
+        $responseStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        
+        switch(substr($responseType, 0, strpos($responseType, ';'))) {
+            case 'application/json':
+                $responseBody = json_decode($responseBody, true);
+            break;
+        }
+
+        if (curl_errno($ch)) {
+            throw new Exception(curl_error($ch));
+        }
+        
+        curl_close($ch);
+
+        if($responseStatus >= 400) {
+            if(is_array($responseBody)) {
+                throw new Exception(json_encode($responseBody));
+            } else {
+                throw new Exception($responseStatus . ': ' . $responseBody);
+            }
+        }
+
+
+        return $responseBody;
+    }
+
+    /**
+     * Flatten params array to PHP multiple format
+     *
+     * @param array $data
+     * @param string $prefix
+     * @return array
+     */
+    protected function flatten(array $data, $prefix = '') {
+        $output = [];
+
+        foreach($data as $key => $value) {
+            $finalKey = $prefix ? "{$prefix}[{$key}]" : $key;
+
+            if (is_array($value)) {
+                $output += $this->flatten($value, $finalKey); // @todo: handle name collision here if needed
+            }
+            else {
+                $output[$finalKey] = $value;
+            }
+        }
+
+        return $output;
+    }
 
 }
