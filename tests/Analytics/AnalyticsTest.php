b0e5bc81ca9daa01f2aaa9f5bf7a0eae6eccc538
Bradley Schofield <ionicisere@gmail.com>
2022-07-04 10:31:34 +0100

    Add tests

--- tests/Analytics/AnalyticsTest.php
+++ tests/Analytics/AnalyticsTest.php
@@ -18,6 +18,7 @@ use PHPUnit\Framework\TestCase;
 use Utopia\Analytics\Adapter\ActiveCampaign;
 use Utopia\Analytics\Adapter\GoogleAnalytics;
 use Utopia\Analytics\Adapter\Plausible;
+use Utopia\Analytics\Adapter\Orbit;
 use Utopia\Analytics\Event;
 
 class AnalyticsTest extends TestCase
@@ -33,11 +34,10 @@ class AnalyticsTest extends TestCase
             getenv("AC_KEY"), 
             getenv("AC_ACTID"),
             getenv("AC_APIKEY"),
-            getenv("AC_ORGID"),
-            "test@test.com");
+            getenv("AC_ORGID"));
         $this->pa = new Plausible(getenv("PA_DOMAIN"), getenv("PA_APIKEY"), 
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "192.168.0.1");
-
+        $this->or = new Orbit(getenv("OR_WORKSPACEID"), getenv("OR_APIKEY"), "Utopia Testing Suite");
     }
 
     public function testGoogleAnalytics()
@@ -160,5 +160,15 @@ class AnalyticsTest extends TestCase
      */
     public function testActiveCampaignDeleteAccount($data) {
         $this->assertTrue($this->ac->deleteAccount($data['accountID']));
+    }
+
+    public function testOrbit() {
+        $event = new Event();
+        $event->setType('testEvent')
+            ->setName('testEvent')
+            ->setUrl('https://www.appwrite.io/docs/installation')
+            ->setProps(['category' => 'testEvent', 'email' => 'test@test.com']);
+
+        $this->assertTrue($this->or->send($event));
     }
 }
