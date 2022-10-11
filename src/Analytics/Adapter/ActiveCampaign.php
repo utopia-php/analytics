87868320c6f62f36662d2fa840262f27d760cd2e
aaryan2134 <aaryan2134@gmail.com>
2022-10-11 16:10:12 +0530

    Updated Authors

--- src/Analytics/Adapter/ActiveCampaign.php
+++ src/Analytics/Adapter/ActiveCampaign.php
@@ -10,7 +10,6 @@
  * @subpackage Tests
  *
  * @link https://github.com/utopia-php/framework
- * @author Torsten Dittmann <torsten@appwrite.io>
  * @version 1.0 RC1
  * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
  */
