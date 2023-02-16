d0180c06af20f1ae84bfd5a0bca85658cbbc43b4
Christy Jacob <christyjacob4@gmail.com>
2023-02-16 13:45:57 +0000

    feat: resolve merge conflicts

--- tests/Analytics/AnalyticsTest.php
+++ tests/Analytics/AnalyticsTest.php
@@ -42,15 +42,6 @@ class AnalyticsTest extends TestCase
     {
         $this->ga = new GoogleAnalytics(App::getEnv("GA_TID"), App::getEnv("GA_CID"));
         $this->ac = new ActiveCampaign(
-<<<<<<< HEAD
-            getenv("AC_KEY"),
-            getenv("AC_ACTID"),
-            getenv("AC_APIKEY"),
-            getenv("AC_ORGID"));
-        $this->pa = new Plausible(getenv("PA_DOMAIN"), getenv("PA_APIKEY"),
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "192.168.0.1");
-        $this->orbit = new Orbit(getenv("OR_WORKSPACEID"), getenv("OR_APIKEY"), "Utopia Testing Suite");
-=======
             App::getEnv("AC_KEY"),
             App::getEnv("AC_ACTID"),
             App::getEnv("AC_APIKEY"),
@@ -58,7 +49,6 @@ class AnalyticsTest extends TestCase
         );
         $this->pa = new Plausible(App::getEnv("PA_DOMAIN"), App::getEnv("PA_APIKEY"), "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "192.168.0.1");
         $this->orbit = new Orbit(App::getEnv("OR_WORKSPACEID"), App::getEnv("OR_APIKEY"), "Utopia Testing Suite");
->>>>>>> f0306021de06277309d2f121bc1860a639c852b5
     }
 
     public function testGoogleAnalytics(): void
@@ -92,45 +82,6 @@ class AnalyticsTest extends TestCase
             ->setUrl('https://www.appwrite.io/docs/pageview123');
 
         $normalEvent = new Event();
-<<<<<<< HEAD
-        $normalEvent->setType('myCoolEvent')
-            ->setUrl('https://www.appwrite.io/docs/myCoolEvent123')
-            ->setProps(['category' => 'coolEvent']);;
-
-         $this->assertTrue($this->pa->createEvent($pageviewEvent));
-         $this->assertTrue($this->pa->createEvent($normalEvent));
-
-         $this->pa->disable();
-         $this->assertFalse($this->pa->createEvent($pageviewEvent));
-         $this->assertFalse($this->pa->createEvent($normalEvent));
-    }
-
-    public function testActiveCampaignCreateAccount(): void {
-        $this->assertTrue($this->ac->createAccount(
-            'Appwrite',
-            'appwrite.io',
-            1,
-            [
-                [
-                    'customFieldId' => 1,
-                    'fieldValue' => 'Hello World!'
-                ],
-            ]
-        ));
-    }
-
-    public function testActiveCampaignGetAccount() {
-        $accountID = $this->ac->accountExists('Appwrite');
-        $this->assertIsNumeric($accountID);
-
-        return [
-            'accountID' => $accountID,
-        ];
-    }
-
-    public function testActiveCampaignCreateContact(): void {
-        $this->assertTrue($this->ac->createContact('test@test.com', 'Paul', 'Van Doren'));
-=======
         $normalEvent->setType('testEvent')
             ->setName('testEvent'.chr(mt_rand(97, 122)).substr(md5(time()), 1, 5))
             ->setUrl('https://www.appwrite.io/docs/installation')
@@ -143,7 +94,6 @@ class AnalyticsTest extends TestCase
 
     public function testActiveCampaignCreateContact() {
         $this->assertTrue($this->ac->createContact('analytics2@utopiaphp.com', 'Analytics', 'Utopia'));
->>>>>>> f0306021de06277309d2f121bc1860a639c852b5
     }
 
     public function testActiveCampaignGetContact() {
@@ -162,9 +112,6 @@ class AnalyticsTest extends TestCase
     /**
      * @depends testActiveCampaignGetContact
      */
-<<<<<<< HEAD
-    public function testActiveCampaignSyncAsociation($data): void{
-=======
     public function testActiveCampaignGetAccount($data) {
         $accountID = $this->ac->accountExists('Example Account 1');
         $this->assertIsNumeric($accountID);
@@ -178,7 +125,6 @@ class AnalyticsTest extends TestCase
      * @depends testActiveCampaignGetAccount
      */
     public function testActiveCampaignSyncAsociation($data) {
->>>>>>> f0306021de06277309d2f121bc1860a639c852b5
         $this->assertTrue($this->ac->syncAssociation($data['accountID'], $data['contactID'], 'Owner'));
         $this->assertTrue($this->ac->syncAssociation($data['accountID'], $data['contactID'], 'Software Developer'));
     }
@@ -186,21 +132,12 @@ class AnalyticsTest extends TestCase
     /**
      * @depends testActiveCampaignGetContact
      */
-<<<<<<< HEAD
-    public function testActiveCampaignUpdateContact($data): void {
-        $this->assertTrue($this->ac->updateContact($data['contactID'], 'test@test.com', '', '', '7223224241'));
-    }
-
-    public function testActiveCampaignDeleteContact(): void {
-        $this->assertTrue($this->ac->deleteContact('test@test.com'));
-=======
     public function testActiveCampaignUpdateContact($data) {
         $this->assertTrue($this->ac->updateContact($data['contactID'], 'analytics2@utopiaphp.com', '', '', '7223224241'));
     }
 
     public function testActiveCampaignDeleteContact() {
         $this->assertTrue($this->ac->deleteContact('analytics2@utopiaphp.com'));
->>>>>>> f0306021de06277309d2f121bc1860a639c852b5
     }
 
     /**
@@ -221,9 +158,6 @@ class AnalyticsTest extends TestCase
         $this->assertTrue($this->ac->deleteAccount($data['accountID']));
     }
 
-<<<<<<< HEAD
-    public function testOrbit(): void {
-=======
     public function testActiveCampaign() {
         $this->assertTrue($this->ac->createContact('analytics@utopiaphp.com', 'Analytics', 'Utopia'));
 
@@ -240,7 +174,6 @@ class AnalyticsTest extends TestCase
 
     public function testOrbit(): void
     {
->>>>>>> f0306021de06277309d2f121bc1860a639c852b5
         $event = new Event();
         $event->setType('testEvent')
             ->setName('testEvent')
@@ -250,8 +183,6 @@ class AnalyticsTest extends TestCase
         $this->assertTrue($this->orbit->send($event));
         $this->assertTrue($this->orbit->validate($event));
     }
-<<<<<<< HEAD
-=======
 
     function testCleanup(): void
     {
@@ -267,5 +198,4 @@ class AnalyticsTest extends TestCase
             $this->assertTrue($this->ac->deleteAccount($this->ac->accountExists('Example Account 1')));
         }
     }
->>>>>>> f0306021de06277309d2f121bc1860a639c852b5
 }
