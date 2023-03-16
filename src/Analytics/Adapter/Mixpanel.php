7fbcaa2c752e2705c0379424286fb2899fbc71aa
Christy Jacob <christyjacob4@gmail.com>
2023-03-16 13:34:45 +0530

    feat: update error messages

--- src/Analytics/Adapter/Mixpanel.php
+++ src/Analytics/Adapter/Mixpanel.php
@@ -70,7 +70,7 @@ class Mixpanel extends Adapter
         $res = $this->call('POST', '/track', $headers, $payload);
 
         if ($res !== '1') {
-            throw new Exception('Failed to send event for ' . $event->getProp('email'));
+            throw new Exception('Failed to send event for '.$event->getProp('email'));
         }
 
         return true;
@@ -92,7 +92,7 @@ class Mixpanel extends Adapter
         $res = $this->call('POST', '/engage#profile-set', $headers, $payload);
 
         if ($res !== '1') {
-            throw new Exception('Failed to create Mixpanel profile for ' . $distinctId);
+            throw new Exception('Failed to create Mixpanel profile for '.$distinctId);
         }
 
         return true;
@@ -114,7 +114,7 @@ class Mixpanel extends Adapter
         $res = $this->call('POST', '/engage#profile-union', $headers, $payload);
 
         if ($res !== '1') {
-            throw new Exception('Failed to append properties for ' . $distinctId);
+            throw new Exception('Failed to append properties for '.$distinctId);
         }
 
         return true;
