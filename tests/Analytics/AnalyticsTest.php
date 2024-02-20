e6d11109f199e13be3eef2aca6519c768cdb5761
Bradley Schofield <ionicisere@gmail.com>
2024-02-20 12:24:44 +0000

    Use Http::getEnv

--- tests/Analytics/AnalyticsTest.php
+++ tests/Analytics/AnalyticsTest.php
@@ -9,6 +9,7 @@ use Utopia\Analytics\Adapter\Mixpanel;
 use Utopia\Analytics\Adapter\Orbit;
 use Utopia\Analytics\Adapter\Plausible;
 use Utopia\Analytics\Event;
+use Utopia\Http\Http;
 
 class AnalyticsTest extends TestCase
 {
@@ -29,11 +30,11 @@ class AnalyticsTest extends TestCase
 
     public function setUp(): void
     {
-        $this->ga = new GoogleAnalytics(getenv('GA_TID') ?? '', getenv('GA_CID') ?? '');
-        $this->pa = new Plausible(getenv('PA_DOMAIN') ?? '', getenv('PA_APIKEY') ?? '', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36', '192.168.0.1');
-        $this->orbit = new Orbit(getenv('OR_WORKSPACEID') ?? '', getenv('OR_APIKEY') ?? '', 'Utopia Testing Suite');
-        $this->mp = new Mixpanel(getenv('MP_PROJECT_TOKEN') ?? '');
-        $this->hs = new HubSpot(getenv('HS_APIKEY') ?? '');
+        $this->ga = new GoogleAnalytics(Http::getEnv('GA_TID') ?? '', Http::getEnv('GA_CID') ?? '');
+        $this->pa = new Plausible(Http::getEnv('PA_DOMAIN') ?? '', Http::getEnv('PA_APIKEY') ?? '', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36', '192.168.0.1');
+        $this->orbit = new Orbit(Http::getEnv('OR_WORKSPACEID') ?? '', Http::getEnv('OR_APIKEY') ?? '', 'Utopia Testing Suite');
+        $this->mp = new Mixpanel(Http::getEnv('MP_PROJECT_TOKEN') ?? '');
+        $this->hs = new HubSpot(Http::getEnv('HS_APIKEY') ?? '');
     }
 
     /**
