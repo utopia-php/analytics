a7c1cd9e76f36329bdd7bd7d19e66fc7ee17dd00
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-04-17 10:28:52 +0000

    feat: add reodev adapter

---
+++ src/Analytics/Adapter/ReoDev.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Utopia\Analytics\Adapter;
+
+use Utopia\Analytics\Adapter;
+use Utopia\Analytics\Event;
+
+class ReoDev extends Adapter
+{
+    /**
+     * Endpoint for ReoDev API
+     */
+    public string $endpoint = 'https://integration.reo.dev/api/';
+
+    /**
+     * API Key
+     */
+    private string $apiKey;
+
+    /**
+     * List ID
+     */
+    private string $listId;
+
+    /**
+     * Gets the name of the adapter.
+     */
+    public function getName(): string
+    {
+        return 'ReoDev';
+    }
+
+    /**
+     * @return ReoDev
+     */
+    public function __construct(string $apiKey, string $listId)
+    {
+        $this->apiKey = $apiKey;
+        $this->listId = $listId;
+    }
+
+    /**
+     * Adds a new developer on the remote analytics platform. Requires email prop.
+     */
+    public function send(Event $event): bool
+    {
+        if (! $event->getProp('email')) {
+            return false;
+        }
+
+        $tags = is_array($event->getProp('tags')) ? $event->getProp('tags') : [];
+
+        if ($event->getProp('account')) {
+            $tags[] = $event->getProp('account');
+        }
+
+        if ($event->getProp('code')) {
+            $tags[] = $event->getProp('code');
+        }
+
+        $entities = [
+            'primaryKey' => $event->getProp('email'),
+            'clientKey' => 'email',
+            'fieldType' => 'String',
+            'companyData' => [
+                'name' => $event->getProp('name'),
+                'tags' => $tags,
+            ],
+        ];
+
+        $this->call('POST', $this->endpoint.'/product/list/'.$this->listId, [
+            'Content-Type' => 'application/json',
+            'x-api-key' => $this->apiKey,
+        ], [
+            'entities' => $entities,
+        ]);
+
+        return true;
+    }
+
+    /**
+     * Sets the client IP address.
+     *
+     * @param  string  $ip  The IP address to use.
+     */
+    public function setClientIP(string $clientIP): self
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    /**
+     * Sets the client user agent.
+     *
+     * @param  string  $userAgent  The user agent to use.
+     */
+    public function setUserAgent(string $userAgent): self
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function validate(Event $event): bool
+    {
+        if (! $this->enabled) {
+            return false;
+        }
+
+        if (empty($event->getType())) {
+            throw new \Exception('Event type is required');
+        }
+
+        if (empty($event->getUrl())) {
+            throw new \Exception('Event URL is required');
+        }
+
+        if (empty($event->getName())) {
+            throw new \Exception('Event name is required');
+        }
+
+        if (empty($event->getProp('email'))) {
+            throw new \Exception('Event email is required');
+        }
+
+        if (! $this->send($event)) {
+            throw new \Exception('Failed to send event');
+        }
+
+        // Check if event made it.
+        $listMembers = $this->call('GET', '/members/find', [
+            'Authorization' => 'Bearer '.$this->apiKey,
+        ], [
+            'source' => 'email',
+            'email' => $event->getProp('email'),
+        ]);
+
+        $listMembers = json_decode($listMembers, true);
+
+        if (empty($listMembers['data'])) {
+            return false;
+        }
+
+        $member = $listMembers['data'];
+
+        $activities = $this->call('GET', '/members/'.$member['id'].'/activities', [
+            'Authorization' => 'Bearer '.$this->apiKey,
+        ], [
+            'activity_type' => $event->getType(),
+        ]);
+
+        $activities = json_decode($activities, true);
+
+        if (empty($activities['data'])) {
+            throw new \Exception('Failed to find event in Orbit');
+        }
+
+        $foundActivity = false;
+
+        foreach ($activities['data'] as $activity) {
+            if ($activity['attributes']['custom_title'] === $event->getName()) {
+                $foundActivity = $activity['id'];
+            }
+        }
+
+        if (! $foundActivity) {
+            throw new \Exception('Failed to find event in Orbit');
+        }
+
+        $this->call('DELETE', '/members/'.$member['id'].'/activities/'.$foundActivity, [
+            'Authorization' => 'Bearer '.$this->apiKey,
+        ], []);
+
+        return true;
+    }
+}
